827ecc80ce2e03ad7c55c8576ee30d2a52a9a218
Joe Guss <joeguss@yahyeet.local>
2022-10-14 13:20:12 +0200

    hey cutie

--- file.py
+++ file.py
@@ -1,4 +1,4 @@
 def apple(a, b):
     return a + b
 
-print("Hey Papi Willy Wonka, can i eat some of your chocolate please")
+print("Hey Papi Willy Wonka, can i eat some of your chocolate pretty please")
